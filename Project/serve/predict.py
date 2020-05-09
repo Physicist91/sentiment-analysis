2cf84e1f689c59dd0976d759e74fad28ddb1416f
Kevin Siswandi <kevinsiswandi@gmail.com>
2020-05-09 08:41:59 +0000

    feat: configure predict in serve

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -69,10 +69,9 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-
-    data_X = None
-    data_len = None
-
+    words = review_to_words(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict, words)
+    
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
     data_pack = np.hstack((data_len, data_X))
@@ -87,6 +86,7 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    result = None
+    result = model(data_pack)
+    result = round(result)
 
     return result
